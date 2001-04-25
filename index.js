4d3659b69d11e4d1ee349d181d00c60ca652ef81
FernandoDLT <ferchodlt1971@gmail.com>
2001-04-25 13:35:01 -0500

    me VS JS wars #63

--- index.js
+++ index.js
@@ -984,3 +984,19 @@ function addSecondLowAndSecondHi(arr) {
 }
 
 addSecondLowAndSecondHi([4, 1, 6, 7, 9, 11, 720])
+
+function fizzBuzz(num) {
+   for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log("FIzBuzz")
+      } else if (i % 3 === 0) {
+         console.log("Fizz")
+      } else if (i % 5 === 0) {
+         console.log("Buzz")
+      } else {
+         console.log(i)
+      }
+   }
+}
+
+fizzBuzz(30)
