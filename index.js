45e24218b46407a082119ab15098ddba52e89246
FernandoDLT <ferchodlt1971@gmail.com>
2025-01-24 17:45:25 -0600

    Code warz me vs JS #1

---
+++ index.js
@@ -0,0 +1,18 @@
+// Start: JavaScript Fundamentals — let, const, scope
+
+// #1
+// Variable 'x' is declared and assigned the
+// value of 10 in the outer/global scope
+let x = 10
+
+// 'if' condition is hardcoded to true so the block
+// always excecutes
+if (true) {
+   // New variable 'x' is shadowed and scoped inside this block,
+   // assigned 20
+   let x = 20
+   // logs 20 (the block scoped 'x')
+   console.log(x)
+}
+// logs 10 (the outer-scoped 'x', completely untouched by the block condition)
+console.log(x)
