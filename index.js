f24a2e22654035bda800d02248609c50489d49f4
FernandoDLT <ferchodlt1971@gmail.com>
2025-04-07 12:49:19 -0500

    Me Vs JS wars #66B

--- index.js
+++ index.js
@@ -1093,3 +1093,19 @@ for (let i = 1; i <= 5; i++) {
 
   console.log(i);
 }
+
+function fizzBucks(num) {
+   for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log("FizzBucks")
+      } else if (i % 3 === 0) {
+         console.log("Fizz")
+      } else if (i % 5 === 0) {
+         console.log("Bucks")
+      } else {
+         console.log(i)
+      }
+   }
+}
+
+fizzBucks(60)
